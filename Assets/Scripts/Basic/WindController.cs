e2fc366d450a88357adc18f1e848a3d5ee2bf9f8
Zhenghao Li <lizhenghao99@outlook.com>
2020-07-09 19:43:51 -0700

    Leaf Wind Material Implemented

--- Assets/Scripts/Basic/WindController.cs
+++ Assets/Scripts/Basic/WindController.cs
@@ -7,16 +7,32 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class WindController : MonoBehaviour
 {
-    public Material[] materials;
+    public Material[] shortGrass;
+    public Material[] highGrass;
+    public Material[] leaves;
 
     [Range(0f, 2f)]
-    public float windStrength;
+    public float shortGrassWindStrength;
+    [Range(0f, 2f)]
+    public float highGrassWindStrength;
+    [Range(0f, 0.2f)]
+    public float leavesWindStrength;
 
     private void Update()
     {
-        foreach (Material m in materials)
+        foreach (Material m in shortGrass)
         {
-            m.SetFloat("_WindStrength", windStrength);
+            m.SetFloat("_WindStrength", shortGrassWindStrength);
+        }
+
+        foreach (Material m in highGrass)
+        {
+            m.SetFloat("_WindStrength", highGrassWindStrength);
+        }
+
+        foreach (Material m in leaves)
+        {
+            m.SetFloat("_WindStrength", leavesWindStrength);
         }
     }
 }
